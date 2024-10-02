efcba0f8efc415eb449b864b832903c6d4a793d6
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2024-10-02 12:36:17 -0300

    instalacion de sekt.io y configuracion basica para el servidor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
+import http from 'http'
+//import https from 'https'
+import { Server as SocketServer } from 'socket.io'
 import app from './app.js';
 
 //setting
 app.set('port', process.env.PORT || 3000)
 
+//http[s] server
+const server = http.createServer(app);
+/*const server = https.createServer({    
+    pfx: fs.readFileSync( './certs/mi_certificado.pfx'),    
+    passphrase: 'Josefina123'
+}, app)*/
+
+//Socket.io
+const io = new SocketServer(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        //methods: ["GET", "POST"]
+    }
+})
+
+//listener
+io.on('connection', (socket) => {
+    console.log('Usuario conectado', socket.id)
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado', socket.id)
+    })
+    socket.on('mensaje', (data) => {
+        console.log(data)
+        socket.broadcast.emit('mensaje', {
+            body: 'Hola a todos',
+            from: socket.id
+        })
+        //io.emit('mensaje', data)
+    })
+})
+
 // Iniciar servidor
-app.listen(app.get('port'), () => {
+//app.listen(app.get('port'), () => {
+server.listen(app.get('port'), () => {
     console.log('Server UP on port', app.get('port'));    
 })
