232255bc8678f45ae48b10778c170e4946a6c527
ArkanHELLCL~ <lcastillop1975@hotmail.com.com>
2024-10-03 18:16:11 -0300

    Modificacion en llamada de registros de bandeja de salida

--- src/controllers/bs.controllers.js
+++ src/controllers/bs.controllers.js
@@ -1,36 +1,73 @@
+import jwt from 'jsonwebtoken'
 import connection from '../database/db.js'
 
-//Bandeja de salida
+//Bandeja de entrada
 export const getBS = async (req, res) => {
-    const { PageNumber, RowsOfPage } = req.query
-    const { usrCod, usrIdentificadorSender } = req.body
-    if(!PageNumber || !RowsOfPage || !usrCod || !usrIdentificadorSender){
-        res.status(404).json({"error":404,message:"Recurso no encontrado"});
+    // Crear schema para validar los datos de entrada
+    const token = req.cookies.access_token
+    if(!token){
+        res.status(403).json({"error":401,message:"No autorizado"});
         return;
     }
-    try {        
-        const pool = await connection();
-        const result = await pool
-                .request()
-                .input("PageNumber", PageNumber)
-                .input("RowsOfPage", RowsOfPage)
-                .input("usrCod",usrCod)
-                .input("usrIdentificadorSender",usrIdentificadorSender)
-                .query("exec [spDatoRequerimientoBSJSON_Listar] @PageNumber, @RowsOfPage, @usrCod, @usrIdentificadorSender");
-        res.status(200).json(result.recordset)    
+    const { PageNumber, RowsOfPage } = req.query
+    if(!PageNumber || !RowsOfPage){
+        res.status(404).json({"error":404,message:"Recurso no encontrado"});
+        return;
+    }    
+
+    //Rescatando datos del payload del token
+    try {
+        const datajwt = jwt.verify(token, process.env.JWT_SECRETO)        
+        const { usrId, usrIdentificadorSender } = datajwt
+        
+        const data =  
+            [{
+                "id" : "bs",
+                "registros": null
+            }]    
+
+        //try {
+            const pool = await connection()
+            const result = await pool
+                    .request()
+                    .input("PageNumber", PageNumber)
+                    .input("RowsOfPage", RowsOfPage)
+                    .input("usrId",usrId)
+                    .input("usrIdentificadorSender",usrIdentificadorSender)
+                    .query("exec [spDatoRequerimientoBSJSON_Listar] @PageNumber, @RowsOfPage, @usrId, @usrIdentificadorSender");
+            data.find(el => el.id === 'be').registros = result.recordset
+            res.status(200).json(data)
+        /*} catch (error) {
+            res.status(500).json({"error":500,message:error.message});
+        }*/
+
     } catch (error) {
         res.status(500).json({"error":500,message:error.message});
-    }
+    }    
 }
 
 export const getBSid = async (req, res) => {
-    res.status(200).json({"message":"Obteniento datos de bandeja de salida con id: "+req.params.id})
+    const { id } = req.params
+    res.status(200).json({"message":"Obteniento datos de bandeja de salida con id: " + id})
 }
 
 export const postBS = async (req, res) => {
+    // Ejemplo de validación de datos con zod
+    // Se debe validar todos los campos que se reciben
+    const result = await validateReq(req.body)
+    if (!result.success) {
+        res.status(400).json({ "error": 400, "message": JSON.parse(result.error.message )})
+        return
+    }
     res.status(201).json({"message":"Guardando datos de bandeja de entrada"})
 }
 
 export const putBSid = (req, res) => {
-    res.status(201).json({"message":"Actualizando datos de bandeja de entrada con id: "+req.params.id})
+    const { id } = req.params
+    const result = validatePartialReq(req.body)
+    if (!result.success) {
+        res.status(400).json({ "error": 400, "message": JSON.parse(result.error.message )})
+        return
+    }
+    res.status(201).json({"message":"Actualizando datos de bandeja de entrada con id: " + id})
 }
